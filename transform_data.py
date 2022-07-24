8ee905ab795b25f26d57c880d2d81b62cb42b35a
Poojith Patnala (patnalap) <patnalap@coventry.ac.uk>
2022-07-24 21:52:28 +0100

    transform_data.py - transformation function

--- transform_data.py
+++ transform_data.py
@@ -6,10 +6,9 @@ from datetime import datetime
 
 dataframe = pd.read_csv('al_results_2020.csv', index_col='index', low_memory=False) #Using pandas to read .csv
 
-
 dataframe = dataframe.drop(columns=['Zscore', 'gender', 'syllabus']) # multiple columns dropped (remove columns: Zscore, gender, syllabus)
 
-dataframe = dataframe.dropna('-') # drops missing values
+#dataframe = dataframe.dropna('-') # drops missing values
 
 dataframe = dataframe.to_csv('new_data_file.csv') #transfer the semi-transformed data to a new file  # ,encoding='utf-8'
 
@@ -34,42 +33,31 @@ def rearranged_date(date):
     return correct_format
 
 
+
 def transform_file(old_file):
 
     updated_results_2020 = open("updated_results_2020.csv", 'w', newline='') # open new file to write the cleaned data
     csv_writer = csv.writer(updated_results_2020, delimiter=',')
 
     headers = ["index","stream","sub1","sub1_r","sub_2", "sub2_r", "sub3", "sub3_r","cgt_r","general_english_r",
-               "birth_date","gender","island_rank","district_rank"] #updated column headers
+               "island_rank","district_rank","birth_date"] #updated column headers
 
-    csv_writer.writerow(headers) # write the headers to new file
-
-#    data = pd.read_csv('al_results_2020.csv')
-#    data.drop('year', inplace=True, axis=1) # dataframe data..
-    # make new .csv after cleaning...
-
-
-
+    # Add headings to new file.
+    csv_writer.writerow(headers)
 
     try:
-        with open(old_file,newline='') as file:
-            csv_reader = csv.reader(file, delimiter=',')
+        with open(old_file, newline='') as file:
+            csv_reader = csv.reader(file, delimiter=",")
 
-            # Continue here.
-            for row in csv_reader:
-                if row is not None:
-
-
+            for row in csv_reader: #select rows to rearrange
+                if rearranged_date(' '.join(row[10:13])): # join all the date rows (birth_day , birth_month, birth_year)
+                    csv_writer.writerow(row[0:10] + row[13:15] + [' '.join(row[10:13])]) # write values to new .csv
 
     except FileNotFoundError as errmsg:
+        print("file not found")
         print(errmsg)
-
     finally:
-        updated_results_2020.close()
+        print("Execution complete")
 
-
-transform_file('new_file.csv')
-
-
-
-
+#Function call
+transform_file('new_data_file.csv')
