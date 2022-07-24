dfc4e0247668e40ceb032e8db3f18a49d3990ed4
Poojith Patnala (patnalap) <patnalap@coventry.ac.uk>
2022-07-24 21:32:18 +0100

    transform_data.py - date function to concatinate date to the required format

--- transform_data.py
+++ transform_data.py
@@ -1,17 +1,37 @@
 import pandas as pd
 import csv
+from datetime import datetime
+
 
 
 dataframe = pd.read_csv('al_results_2020.csv', index_col='index', low_memory=False) #Using pandas to read .csv
 
-# remove columns: Zscore, gender, syllabus
-dataframe = dataframe.drop(columns=['Zscore', 'gender', 'syllabus']) # multiple columns dropped
 
-dataframe = dataframe.dropna() # drops missing values
+dataframe = dataframe.drop(columns=['Zscore', 'gender', 'syllabus']) # multiple columns dropped (remove columns: Zscore, gender, syllabus)
+
+dataframe = dataframe.dropna('-') # drops missing values
 
 dataframe = dataframe.to_csv('new_data_file.csv') #transfer the semi-transformed data to a new file  # ,encoding='utf-8'
 
 
+
+def rearranged_date(date):
+    '''
+
+    :param date: current date values provided
+    :return: a formatted date in the "DD:MONTH:YYYY"
+
+    '''
+
+    try:
+        date_and_time = datetime.strptime(date, '%d %B %Y') #string to date time object format
+        correct_format = True
+        print("correct data format")
+
+    # Catches invalid date entries
+    except ValueError:
+        correct_format = False
+    return correct_format
 
 
 def transform_file(old_file):
